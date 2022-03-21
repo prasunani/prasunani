7cf9ddcec4e1ea2f916a2ec0a35c7d61f52c3451
Vinla <Vinla@LAPTOP-M2UO22U7.roc.mn.charter.com>
2022-03-21 15:00:45 -0500

    my first commit  to local repository

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+		String x="ufbukfhbk#$^784FHJ";
+		String y=x.replaceAll("[^0-9]","");
+		System.out.println(y);
+
+	}
+
+}
